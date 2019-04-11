0c0b525143501cc11a6e51789cd455d2438a1ef8
ZlatinaMihaylova <zlatina.mihaylova@abv.bg>
2019-04-11 17:03:22 +0300

    Update Player.java

--- homework/Player.java
+++ homework/Player.java
@@ -1,12 +1,10 @@
-package homework;
+package main.homework;
+
+import java.util.Arrays;
 
 public final class Player {
 
     private static final int BEGINNING_POINTS = 0;
-    private static final String ZERO_POINTS_RESULT = "Love";
-    private static final String ONE_POINT_RESULT = "Fifteen";
-    private static final String TWO_POINTS_RESULT = "Thirty";
-    private static final String THREE_POINTS_RESULT = "Forty";
     private static final int WINNING_DIFFERENCE = 2;
     private static final int WINNING_POINTS = 4;
 
@@ -16,25 +14,17 @@ public final class Player {
 
     public Player(String name) {
         this.name = name;
-        this.result = "";
+        this.result = new String();
         this.points = Player.BEGINNING_POINTS;
     }
 
+    void increasePointsAndResult(){
+        this.points++;
+        this.changeResult();
+    }
+
     private void changeResult() {
-        switch(this.points){
-            case 0:
-                this.result = Player.ZERO_POINTS_RESULT;
-                break;
-            case 1:
-                this.result = Player.ONE_POINT_RESULT;
-                break;
-            case 2:
-                this.result = Player.TWO_POINTS_RESULT;
-                break;
-            case 3:
-                this.result = Player.THREE_POINTS_RESULT;
-                break;
-        }
+        this.result = ScoringSystem.getResult(this.points);
     }
 
     boolean hasAdvantage(Player player){
@@ -45,13 +35,8 @@ public final class Player {
         return this.points >= Player.WINNING_POINTS && (this.points - player.getPoints()) >= Player.WINNING_DIFFERENCE;
     }
 
-    private boolean hasMorePoints(Player player){
+    boolean hasMorePoints(Player player){
         return this.points > player.getPoints();
-    }
-
-    void increasePointsAndResult(){
-        this.points++;
-        this.changeResult();
     }
 
     String getName() {
@@ -64,5 +49,29 @@ public final class Player {
 
     int getPoints() {
         return this.points;
+    }
+
+    enum ScoringSystem{
+        
+        ZERO_POINTS_RESULT ("Love", 0),
+        ONE_POINT_RESULT ("Fifteen", 1),
+        TWO_POINTS_RESULT ("Thirty", 2),
+        THREE_POINTS_RESULT ("Forty", 3);
+
+        private String result;
+        private int points;
+
+        ScoringSystem(final String result, final int points) {
+            this.result = result;
+            this.points = points;
+        }
+
+        static String getResult(int points){
+            return Arrays.stream(ScoringSystem.values())
+                    .filter( scoringSystem -> scoringSystem.points == points)
+                    .map( scoringSystem -> scoringSystem.result)
+                    .findFirst()
+                    .get();
+        }
     }
 }
